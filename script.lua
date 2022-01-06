8eefb064eb6a8c87474792b9c2fe0b91930577c9
AAGNGT <tingting2389@gmail.com>
2022-01-06 17:31:38 +0800

    SCRIPT 大更新

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@
 local ScriptGUI = Instance.new("ScreenGui")
 local Sound = Instance.new("Sound")
 
---PasswordGui
+--Password
 local PasswordFrame = Instance.new("Frame")
 local PasswordFrame_2 = Instance.new("Frame")
 local HiText = Instance.new("TextLabel") 
@@ -11,12 +11,95 @@ local PassBox_2 = Instance.new("TextBox")
 local PassBtn = Instance.new("TextButton")
 local exit = Instance.new("TextButton")
 
+--JoinGame
+local OPENJoin = Instance.new("TextButton")
+local JoinGameFrame = Instance.new("Frame")
+local Join_UIGradient = Instance.new("UIGradient")
+local Join_UICorner = Instance.new("UICorner")
+local JoinText = Instance.new("TextLabel")
+local TextLabel = Instance.new("TextLabel")
+
 
 ScriptGUI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Sound.Parent = ScriptGUI
 Sound.SoundId = "rbxassetid://1673280232"
 
+--OPENBtn
+OPENJoin.Parent = ScriptGUI
+OPENJoin.BackgroundColor3 = Color3.fromRGB(116, 116, 116)
+OPENJoin.BorderSizePixel = 0
+OPENJoin.Position = UDim2.new(0, 0, 0.144, 0)
+OPENJoin.Size = UDim2.new(0, 276, 0, 18)
+OPENJoin.Font = Enum.Font.SourceSans
+OPENJoin.Text = "<"
+OPENJoin.TextColor3 = Color3.fromRGB(0, 0, 0)
+OPENJoin.TextScaled = true
+OPENJoin.TextSize = 14.000
+OPENJoin.TextWrapped = true
+OPENJoin.Visible = false
+OPENJoin.MouseButton1Click:connect(function()
+	if JoinGameFrame.Position == UDim2.new(0, 0, 0.173, 0) then
+		OPENJoin.Text = ">"
+		JoinGameFrame:TweenPosition(
+			UDim2.new(-1, 0, 0.173, 0),
+			"Out",
+			"Bounce",
+			0.5,
+			false
+		)
+	else
+		OPENJoin.Text = "<"
+		JoinGameFrame:TweenPosition(
+			UDim2.new(0, 0, 0.173, 0),
+			"Out",
+			"Bounce",
+			1,
+			false
+		)
+	end
+end)
+
+--JoinGame
+JoinGameFrame.Name = "JoinGameFrame"
+JoinGameFrame.Parent = ScriptGUI
+JoinGameFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+JoinGameFrame.Position = UDim2.new(-1, 0, 0.173, 0)
+JoinGameFrame.Size = UDim2.new(0, 276, 0, 398)
+JoinGameFrame.Visible = false
+
+Join_UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(149, 149, 149)), ColorSequenceKeypoint.new(0.96, Color3.fromRGB(2, 2, 2)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
+Join_UIGradient.Parent = JoinGameFrame
+
+Join_UICorner.Parent = JoinGameFrame
+
+--JoinText
+JoinText.Name = "JoinText"
+JoinText.Parent = JoinGameFrame
+JoinText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+JoinText.BackgroundTransparency = 1.000
+JoinText.Position = UDim2.new(0.115942031, 0, 0.0100502511, 0)
+JoinText.Size = UDim2.new(0, 200, 0, 50)
+JoinText.Font = Enum.Font.SourceSansBold
+JoinText.Text = "Join game is "
+JoinText.TextColor3 = Color3.fromRGB(153, 153, 153)
+JoinText.TextScaled = true
+JoinText.TextSize = 14.000
+JoinText.TextWrapped = true
+
+TextLabel.Parent = JoinGameFrame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.137681156, 0, 0.135678396, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "BedWars"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+--PasswordFRame
 local UICorner = Instance.new("UICorner")
 local UICorner_2 = Instance.new("UICorner")
 
@@ -119,7 +202,13 @@ PassBtn.MouseButton1Click:connect(function()
 		PasswordFrame:TweenPosition(
 			UDim2.new(0.384, 0, -1, 0)
 		)
-		ScriptGUI:Destroy()
+		wait(1)
+		JoinGameFrame.Visible = true
+		OPENJoin.Visible = true
+		JoinGameFrame:TweenPosition(
+			UDim2.new(0, 0, 0.173, 0)
+		)
+
 	else
 		Sound:Play()
 		PassBtn.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
